81078467bc0a5aa5ffd96e228024126c27b04346
LindaSt <linda.studer@unifr.ch>
2021-09-28 17:50:44 +0200

    fixed issues with edge re-indexing. added option to process whole folder

--- process_folder.py
+++ process_folder.py
@@ -2,8 +2,9 @@ from graph_plotter import graph_plotter
 import fire
 import glob
 import os
+from util.gxl_graph import InvalidFileException
 
-def process_folder(gxl_folder, img_folder, output_path, separator='_', color_by_feature=None, scaling=1, transparency=125):
+def process_folder(gxl_folder, img_folder, output_path, separator='_AE', color_by_feature=None, scaling=1, transparency=125):
     gxl_files = {os.path.basename(f).split(separator)[0]: f for f in glob.glob(os.path.join(gxl_folder, '*.gxl'))}
     img_files = {os.path.basename(f).split(separator)[0]: f for f in glob.glob(os.path.join(img_folder, '*'))}
 
@@ -11,8 +12,12 @@ def process_folder(gxl_folder, img_folder, output_path, separator='_', color_by_
     file_ids = [value for value in gxl_files.keys() if value in img_files.keys()]
 
     for file_id in file_ids:
-        graph_plotter(gxl_files[file_id], img_files[file_id], output_path, color_by_feature=color_by_feature, scaling=scaling, transparency=transparency)
-
+        try:
+            print(file_id)
+            graph_plotter(gxl_files[file_id], img_files[file_id], output_path, color_by_feature=color_by_feature, scaling=scaling, transparency=transparency)
+        except InvalidFileException:
+            print(f'Something went wrong with files {gxl_files[file_id]}/{img_files[file_id]}, skipping...')
+            continue
 
 if __name__ == '__main__':
     fire.Fire(process_folder)
--- util/drawing_config.py
+++ util/drawing_config.py
@@ -1,6 +1,13 @@
 bts_graphs = {
-    'node_style': {'tumorbud': {'color': (38, 210, 255, 255), 'radius': 26},
-                   'lymphocyte': {'color': (99, 68, 212, 255), 'radius': 22},
+    'node_style': {'tumorbud': {'color': (38, 210, 255, 255), 'radius': 12},
+                   'lymphocyte': {'color': (99, 68, 212, 255), 'radius': 10},
                    'thickness': -1},
     'edge_style': {'color': (168, 50, 117, 255), 'thickness': 10}
 }
+
+# bts_graphs = {
+#     'node_style': {'tumorbud': {'color': (38, 210, 255, 255), 'radius': 26},
+#                    'lymphocyte': {'color': (99, 68, 212, 255), 'radius': 22},
+#                    'thickness': -1},
+#     'edge_style': {'color': (168, 50, 117, 255), 'thickness': 10}
+# }
